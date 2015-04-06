96b31da509e3390491431ebe86394599909ae132
catharsist <catharsistk@gmail.com>
2015-04-06 16:12:44 +0900

    Make a base structure for Crossover algorithms

---
+++ src/kim/kilho/ga/algorithm/Crossover.java
@@ -0,0 +1,100 @@
+package kim.kilho.ga.algorithm;
+
+import kim.kilho.ga.gene.Path;
+
+/**
+ * Crossover algorithms for GA.
+ * @author Kilho Kim
+ */
+public class Crossover {
+
+  /**
+   * One-point Crossover.
+   * @param p1
+   * @param p2
+   * @return Path
+   */
+  public static Path onePointCrossover(Path p1, Path p2) {
+    return null;
+  }
+
+  /**
+   * Multi-point Crossover.
+   * @param p1
+   * @param p2
+   * @return Path
+   */
+  public static Path multiPointCrossover(Path p1, Path p2) {
+    return null;
+  }
+
+  /**
+   * Uniform Crossover.
+   * @param p1
+   * @param p2
+   * @return Path
+   */
+  public static Path uniformCrossover(Path p1, Path p2) {
+    return null;
+  }
+
+  /**
+   * Cycle Crossover.
+   * @param p1
+   * @param p2
+   * @return Path
+   */
+  public static Path cycleCrossover(Path p1, Path p2) {
+    return null;
+  }
+
+  /**
+   * Order Crossover.
+   * @param p1
+   * @param p2
+   * @return Path
+   */
+  public static Path orderCrossover(Path p1, Path p2) {
+    return null;
+  }
+
+  /**
+   * Partially Matched Crossover(PMX).
+   * @param p1
+   * @param p2
+   * @return Path
+   */
+  public static Path partiallyMatchedCrossover(Path p1, Path p2) {
+    return null;
+  }
+
+  /**
+   * Arithmetic Crossover.
+   * @param p1
+   * @param p2
+   * @return Path
+   */
+  public static Path arithmeticCrossover(Path p1, Path p2) {
+    return null;
+  }
+
+  /**
+   * Heuristic Crossover.
+   * @param p1
+   * @param p2
+   * @return Path
+   */
+  public static Path heuristicCrossover(Path p1, Path p2) {
+    return null;
+  }
+
+  /**
+   * Edge Recombination.
+   * @param p1
+   * @param p2
+   * @return Path
+   */
+  public static Path edgeRecombination(Path p1, Path p2) {
+    return null;
+  }
+}
--- src/kim/kilho/ga/test/Test.java
+++ src/kim/kilho/ga/test/Test.java
@@ -1,5 +1,6 @@
 package kim.kilho.ga.test;
 
+import kim.kilho.ga.algorithm.Crossover;
 import kim.kilho.ga.algorithm.Selection;
 import kim.kilho.ga.exception.InvalidInputException;
 import kim.kilho.ga.gene.Path;
@@ -31,13 +32,22 @@ public class Test {
   static double timeLimit;
 
   // Constants
+  // Selection
   public static final int ROULETTE_WHEEL_SELECTION = 1;
     public static final double SELECTION_PRESSURE_PARAM = 3;
     public static final double SELECTION_TOURNAMENT_T = 0.9;
-
-
   public static final int TOURNAMENT_SELECTION = 2;
 
+  // Crossover
+  public static final int ONE_POINT_CROSSOVER = 1;
+  public static final int MULTI_POINT_CROSSOVER = 2;
+  public static final int UNIFORM_CROSSOVER = 3;
+  public static final int CYCLE_CROSSOVER = 4;
+  public static final int ORDER_CROSSOVER = 5;
+  public static final int PARTIALLY_MATCHED_CROSSOVER = 6;
+  public static final int ARITHMETIC_CROSSOVER = 7;
+  public static final int HEURISTIC_CROSSOVER = 8;
+  public static final int EDGE_RECOMBINATION = 9;
 
   // How to run:
   // $ java Test data/cycle.in
@@ -88,10 +98,20 @@ public class Test {
         }
         // Select two paths p1 and p2 from population
         Path p1 = selection(ROULETTE_WHEEL_SELECTION);
-        System.out.println("p1: " + p1.toString());
         Path p2 = selection(ROULETTE_WHEEL_SELECTION);
+//        Path p1 = selection(TOURNAMENT_SELECTION);
+//        Path p2 = selection(TOURNAMENT_SELECTION);
+        System.out.println("p1: " + p1.toString());
         System.out.println("p2: " + p2.toString());
-        // Path offspring = crossover(p1, p2);
+        Path offspring = crossover(p1, p2, ONE_POINT_CROSSOVER);
+//        Path offspring = crossover(p1, p2, MULTI_POINT_CROSSOVER);
+//        Path offspring = crossover(p1, p2, UNIFORM_CROSSOVER);
+//        Path offspring = crossover(p1, p2, CYCLE_CROSSOVER);
+//        Path offspring = crossover(p1, p2, ORDER_CROSSOVER);
+//        Path offspring = crossover(p1, p2, PARTIALLY_MATCHED_CROSSOVER);
+//        Path offspring = crossover(p1, p2, ARITHMETIC_CROSSOVER);
+//        Path offspring = crossover(p1, p2, HEURISTIC_CROSSOVER);
+//        Path offspring = crossover(p1, p2, EDGE_RECOMBINATION);
         // offspring = mutation(offspring);
         // Path[] offsprings = null; // TODO: add offsprings into this path array
       // }
@@ -99,7 +119,6 @@ public class Test {
       e.getMessage();
     }
   }
-
 
 
   /**
@@ -112,25 +131,39 @@ public class Test {
       // 1. Roulette Wheel Selection.
       case 1:
         return Selection.rouletteWheelSelection(population,
-                                        SELECTION_PRESSURE_PARAM);
+                SELECTION_PRESSURE_PARAM);
       case 2:
         return Selection.tournamentSelection(population,
-                                        SELECTION_TOURNAMENT_T);
+                SELECTION_TOURNAMENT_T);
       default:
-        throw new Exception("Invalid option input");
+        throw new Exception("Invalid option input.");
     }
-
-
-
-    /*
-    Random rnd = new Random();
-    return population[rnd.nextInt(population.length)];
-    */
   }
 
-  private static Path crossover(Path p1, Path p2) {
-    // TODO:
-    return null;
+  private static Path crossover(Path p1, Path p2, int option) throws Exception {
+    switch(option) {
+      // 1. One-point Crossover
+      case 1:
+        return Crossover.onePointCrossover(p1, p2);
+      case 2:
+        return Crossover.multiPointCrossover(p1, p2);
+      case 3:
+        return Crossover.uniformCrossover(p1, p2);
+      case 4:
+        return Crossover.cycleCrossover(p1, p2);
+      case 5:
+        return Crossover.orderCrossover(p1, p2);
+      case 6:
+        return Crossover.partiallyMatchedCrossover(p1, p2);
+      case 7:
+        return Crossover.arithmeticCrossover(p1, p2);
+      case 8:
+        return Crossover.heuristicCrossover(p1, p2);
+      case 9:
+        return Crossover.edgeRecombination(p1, p2);
+      default:
+        throw new Exception("Invalid option input.");
+    }
   }
 
   private static Path mutation(Path p) {
